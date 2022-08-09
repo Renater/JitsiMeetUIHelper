6084c1ff360b5a6a0e74b97cfdb0e23cc19bc17b
Damien Bruchet <damien.bruchet@renater.fr>
2022-08-09 16:12:26 +0200

    Minor fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,19 @@
 import JitsiMeetUIHelper from './JitsiMeetUIHelper.js';
 import Lang from './modules/Lang.js';
 
+
+
 /* Get translations */
-Lang.init();
+Lang.init()
+    .then(() => {
+        /* Initiate IFrame */
+        var helper = new JitsiMeetUIHelper();
 
-/* Initiate IFrame */
-var helper = new JitsiMeetUIHelper();
+        /* Add listener for click on show DTMF menu */
+        helper.dtmfMenuButton.addEventListener('click', function(){
+            helper.executeCommand('show-dtmf-menu');
+        });
 
-/* Add listener for click on show DTMF menu */
-helper.dtmfMenuButton.addEventListener('click', function(){
-    helper.executeCommand('show-dtmf-menu');
-});
+    });
+
 

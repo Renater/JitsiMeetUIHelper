ee919707d899ae2a16c1d0b3bf20b9da2542240a
Damien Bruchet <damien.bruchet@renater.fr>
2022-09-15 17:34:59 +0200

    Fix gTTS not generating a "real" mp3 file Add audio frame rate customization

--- scripts/generate_tts_files.py
+++ scripts/generate_tts_files.py
@@ -1,17 +1,20 @@
 """
-Generate TTS files for an offline usage
+Generate TTS files for an offline usage.
 
 Prerequisites:
     Install following pip modules:
 
-     pip3 install gTTS-token --upgrade
-     pip3 install gTTS --upgrade
+     pip install gTTS-token --upgrade
+     pip install gTTS --upgrade
+     pip install pydub
 """
 
 import os, json, sys, getopt
 from os import walk
 from gtts import gTTS
 from pathlib import Path
+from pydub import AudioSegment
+
 
 # InputFolder where json lang translation files are
 inputFolder = "./lang/"
@@ -22,6 +25,8 @@ outputFolder = "./gen/"
 # Output format
 format = "mp3"
 
+# Audio frame rate
+frameRate = 27000
 
 # Generic function to generate TTS mp3 files
 def generateTTS(key, value, lang):
@@ -30,22 +35,31 @@ def generateTTS(key, value, lang):
     print("[Lang:",lang,"] Generating file", outputFile)
     gt = gTTS(text=value, lang=lang, slow=False)
     gt.save(outputFile)
+    audioEncode(outputFile, format)
+
+# encode in asked audio file format
+def audioEncode(file, audioFormat):
+    sound = AudioSegment.from_mp3(file)
+    sound.frame_rate = int(frameRate)
+    sound.export(file, format=audioFormat)
+
 
 # Get command line arguments
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hi:o:f:",["input-folder=","output-folder=", "format="])
+    opts, args = getopt.getopt(sys.argv[1:],"hi:o:f:",["input-folder=","output-folder=", "format=", "frame-rate="])
 
 except getopt.GetoptError:
-    print('generate_tts_files.py [-i/--input-folder <input_folder>] [-o/--output-folder <output_folder>] [-f/-format <format>]')
+    print('generate_tts_files.py [-i/--input-folder <input_folder>] [-o/--output-folder <output_folder>] [-f/-format <format>] [--frame-rate <frame_rate]')
     sys.exit(2)
 
 for opt, arg in opts:
     if opt == '-h':
         print('Usage:')
-        print('generate_tts_files.py [-i/--input-folder <input_folder>] [-o/--output-folder <output_folder>] [-f/-format <format>]')
+        print('generate_tts_files.py [-i/--input-folder <input_folder>] [-o/--output-folder <output_folder>] [-f/-format <format>] [--frame-rate <frame_rate>]')
         print('\t-i/--input-folder <input_folder>: input folder where translation json files are')
         print('\t-o/--output-folder <output_folder>: folder where files will be generated')
         print('\t-f/--format <format>: audio file format')
+        print('\t--frame-rate <frame_rate>: audio frame rate. Default is 26000')
         sys.exit()
     elif opt in ("-i", "--input-folder"):
         inputFolder = arg
@@ -53,10 +67,14 @@ for opt, arg in opts:
         outputFolder = arg
     elif opt in ("-f", "--format"):
         format = arg
+    elif opt == "--frame-rate":
+        frameRate = arg
 
 
-print('[Info] InputFolder: ' + inputFolder)
-print('[Info] OutputFolder: ' + outputFolder)
+print('[Info] Input folder: ' + inputFolder)
+print('[Info] Output folder: ' + outputFolder)
+print('[Info] Audio format: ' + format)
+print('[Info] Audio frame rate: ' + str(frameRate))
 
 # Check if input folder exists
 if not os.path.isdir(inputFolder):
@@ -67,7 +85,6 @@ if not os.path.isdir(inputFolder):
 if not os.path.isdir(outputFolder):
     print("[Error] output folder (\""+outputFolder+"\") not found, exiting.")
     sys.exit(2)
-
 
 
 f = []

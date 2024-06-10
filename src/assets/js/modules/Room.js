a18a3527fab43fcdc423e1cd8d16e87f0c59fc61
Nicolas Tizon <nicolas.tizon@gmail.com>
2024-06-10 16:04:38 +0200

    [Fixed] entire screen sharing by default

--- src/assets/js/modules/Room.js
+++ src/assets/js/modules/Room.js
@@ -152,7 +152,10 @@ export default class Room {
                   //  customIcons: {
                   //      SecurityOn: 'https://127.0.0.1:8443/UIhelper/assets/images/security-on_green.svg'
                   //  }
-                    testing : {no_customUI: true}
+                    testing : {no_customUI: true},
+                    screenShareSettings: {
+                        desktopDisplaySurface: 'monitor'
+                    }
                 },
                 parentNode: mainContainer,
             }
